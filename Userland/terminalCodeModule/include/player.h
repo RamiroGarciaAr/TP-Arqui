5cc1fd905fc0e824f9b44184731868b31754a871
RamiroGarciaAr <67532049+RamiroGarciaAr@users.noreply.github.com>
2023-10-25 23:23:56 -0300

    Color & Player Added

--- Userland/terminalCodeModule/include/player.h
+++ Userland/terminalCodeModule/include/player.h
@@ -1,6 +1,8 @@
 #ifndef _PLAYER_H_
 #define _PLAYER_H_
 
+#include <color.h>
+
 typedef struct player
 {
     int points; //amount of apples eaten
@@ -8,7 +10,13 @@ typedef struct player
     
     uint64_t xAxis; // pos X
     uint64_t yAxis; // pos Y
-    
+
+    int moveDir; // 1=up , 2=down, 3=left, 4=right
+    int initialSize; //start of the starting snake
+
+    Color color;
+
+
 }
 
 #endif
