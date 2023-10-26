329651dd181350b5c0a992b036e4df9fa15fa8d4
RamiroGarciaAr <67532049+RamiroGarciaAr@users.noreply.github.com>
2023-10-25 23:10:51 -0300

    Create player.h

---
+++ Userland/terminalCodeModule/include/player.h
@@ -0,0 +1,14 @@
+#ifndef _PLAYER_H_
+#define _PLAYER_H_
+
+typedef struct player
+{
+    int points; //amount of apples eaten
+    bool isAlive; 
+    
+    uint64_t xAxis; // pos X
+    uint64_t yAxis; // pos Y
+    
+}
+
+#endif
